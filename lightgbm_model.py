42191b9623a630845eb0898ce5cc08aff4479e94
PVGorshenin <36691327+PVGorshenin@users.noreply.github.com>
2018-02-21 13:10:04 +0300

    Update lightgbm_model.py

--- lightgbm_model.py
+++ lightgbm_model.py
@@ -1,4 +1,4 @@
-import lightgbm as lgb
+mport lightgbm as lgb
 from time import time
 from scipy.sparse import csr_matrix
 from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer, TfidfTransformer
@@ -6,10 +6,14 @@ from sklearn.metrics import mean_squared_log_error
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import gc
-from sklear
+
+
+def build_preprocessor(field):
+    field_idx = list(train.columns).index(field)
+    return lambda x: default_preprocessor(x[field_idx])
 
 y_train = np.log1p(train["price"])
-df = pd.concat([train,test], 0)
+df = pd.concat([train, test], 0)
 nrow_train = train.shape[0]
 
 
@@ -24,9 +28,6 @@ train = df.iloc[:nrow_train]
 test = df.iloc[nrow_train:]
 
 default_preprocessor = CountVectorizer().build_preprocessor()
-def build_preprocessor(field):
-    field_idx = list(train.columns).index(field)
-    return lambda x: default_preprocessor(x[field_idx])
 
 vectorizer_LG = FeatureUnion([
     ('name', CountVectorizer(
@@ -49,9 +50,9 @@ vectorizer_LG = FeatureUnion([
         preprocessor=build_preprocessor('item_description'))),
 ])
 
-dtrain, dvalid = train_test_split(train, shuffle = False, train_size=0.999)
+dtrain, dvalid = train_test_split(train, shuffle=False, train_size=0.999)
 X_train = vectorizer_LG.fit_transform(dtrain.values)
-X_valid= vectorizer_LG.transform(dvalid.values)
+X_valid = vectorizer_LG.transform(dvalid.values)
 X_test = vectorizer_LG.transform(test.values)
 print('Made data', X_train.shape[0], X_valid.shape[0])
 lgb_train = lgb.Dataset(X_train, label=y_train[:X_train.shape[0]])
@@ -65,10 +66,11 @@ params = {
         'metric': 'RMSE',        
         'bagging_fraction': 0.8,
         'feature_fraction': 0.9,
-         'num_thread':4
+        'num_thread': 4
     }
     
-model_LGB = lgb.train(params, train_set=lgb_train, num_boost_round=1600, verbose_eval=100, valid_sets = lgb_val)   
+model_LGB = lgb.train(params, train_set=lgb_train, num_boost_round=1600,
+                      verbose_eval=100, valid_sets=lgb_val)   
 pred_LGB = np.expm1(model_LGB.predict(X_test))
-pred_LGB[pred_LGB <3] = 3
+pred_LGB[pred_LGB < 3] = 3
 print('preg_LGB', pred_LGB[:10])
